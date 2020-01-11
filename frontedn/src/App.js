c5e2092f7e7780054870aea4f1d87badb35923ab
Ramon <ramon_ro1@hotmail.ccom>
2020-01-11 15:26:37 -0300

    Cod.

---
+++ frontedn/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { Button } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+      <Button color="primary">primary</Button>
+    </div>
+  );
+}
+
+export default App;
